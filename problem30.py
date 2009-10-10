fa06660a69cb82a6dbf1a029262431110942c163
Charles Leifer <coleifer@gmail.com>
2009-10-10 17:17:48 -0500

    Fixed a typo in problem30 that yielded an incorrect max_possible

--- problem30.py
+++ problem30.py
@@ -2,9 +2,10 @@
 # abc.. = a^5 + b^5 + c^5 ...
 max_digit = 9**5 # 59049
 n = len(str(max_digit)) # figure out how long the number is
-while (max_digit*n <= 10**n):
+while (max_digit*n >= 10**n):
     n += 1
 max_possible = max_digit * n # 354294
+print "Max possible: %d" % max_possible
 powers = [x**5 for x in range(0, 10)] # cache powers
 found = []
 for i in range(2, max_possible):
